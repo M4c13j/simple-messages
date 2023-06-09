291db0df084c835254edab7387f8f6cdfea79e00
M4c13j <maciekcx11@gmail.com>
2023-06-09 19:31:01 +0200

    first version no motivation to finish

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
 import socket
 import sys
+import threading
+import tkinter
+
+def logmessage(msg):
+    print(f"[LOG] {msg}")
 
 HOST = "127.0.0.1"
 PORT = 65456
@@ -9,8 +14,91 @@ server = socket.socket(
     socket.SOCK_STREAM
 )
 
-server.connect( (HOST, PORT) )
-server.sendall(b"Penis Kutas siurrrr")
-data = server.recv(1024)
 
-print(f"Recieved {data}")
+
+def listen():
+    while True:
+        msg = server.recv(1024).decode()
+        text_box.insert(tkinter.END, msg)
+        logmessage("Message recieved: "+msg)
+
+def sendMessage(event=None):
+    msg = input_text.get()
+    if len(msg) == 0:
+        logmessage("Attempt of sending an empty message.")
+        return
+    input_text.set("")
+    server.sendall( msg.encode() )
+    text_box.insert(tkinter.END, f"<me> {msg}")
+    logmessage("Message "+msg+" has been sent.")
+
+def setupMessages():
+    text_box.insert( tkinter.END, "Welcome to the chat!")
+    text_box.insert( tkinter.END, "Here are available commands:")
+    text_box.insert( tkinter.END, "/chnick <newnick>")
+
+
+
+
+# GUI related 
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 350
+window = tkinter.Tk()
+window.geometry('350x500')
+window.title("Simple messages")
+
+input_text = tkinter.StringVar()
+# input_text.set("Your message...")
+
+chat_title = tkinter.Label(
+    window,
+    text="Chat messages",
+    font="60"
+)
+
+scrollbar = tkinter.Scrollbar(
+    window
+)
+
+text_box = tkinter.Listbox( 
+    window,
+    yscrollcommand=scrollbar.set,
+    height="25",
+    width="50"
+)
+
+send_button = tkinter.Button( 
+    window, 
+    text="Send",
+    command=sendMessage
+)
+
+text_field = tkinter.Entry( 
+    window,
+    textvariable=input_text,
+    width="40",
+)
+text_field.bind("<Return>", sendMessage)
+
+chat_title.pack( side=tkinter.TOP, anchor="center" )
+text_box.pack( side=tkinter.TOP, anchor="center")
+send_button.place( x=300, y=450)
+text_field.place( x=20, y=450 )
+
+setupMessages()
+
+
+# connecting to the server
+logmessage(f"Connecting to {HOST} {PORT}")
+server.connect( (HOST, PORT) )
+logmessage(f"Connection established.")
+
+# background message recieving thread
+background = threading.Thread(target=listen)
+background.daemon = True
+background.start()
+
+
+window.mainloop()
+
+server.close()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-import _thread
+import threading
 import time
 
 HOST = "127.0.0.1"
@@ -14,34 +14,70 @@ server = socket.socket(
 server.bind( (HOST, PORT) )
 server.listen()
 server_start_time = time.strftime("%X %x")
-print(f"Started server ({HOST},{PORT})   time: {server_start_time}")
+print(f"Started server ({HOST},{PORT}) \n Start time: {server_start_time}")
 
-list_of_clients = []
+list_of_clients = [] #
+nicknames = {}
 
-def handle_client(conn):
-    conn.send("Sup bro")
+def userId( conn ):
+    # user id is a hash of ip and port 
+    return hash( (conn.getpeername()[0],conn.getpeername()[1]) )
 
+# handle server log messages
+def server_log(msg):
+    print(f"[SERVER] {msg}")
+
+# post messages from clients to chat
+def postMessage(msg, conn):
+    return f"<{nicknames[userId(conn)]}> {msg}"
+
+def broadcast(msg,conn):
+    msg = msg.encode()
+    for client in list_of_clients:
+        if client != conn:
+            client.sendall( msg )
+
+def handle_client(conn: socket.socket):
     while True:
         try:
-            msg = conn.recv(1024)
-            if msg:
-                print(f"<{conn.getpeername()[0]}>: {msg}")
-            else:
-                remove(conn)
+            msg = conn.recv(1024).decode()
         except:
+            remove(conn)
+            break
+
+        if msg.startswith("NICKCHANGE"):
+            nicknames[userId(conn)] = msg[10:]
+            server_log("Nickname changed to"+nicknames[userId(conn)])
+            broadcast("Nickname changed to"+nicknames[userId(conn)], None)
             continue
+        else:
+            tosend = postMessage(msg, conn)
+            broadcast( tosend, conn )
+
+    server_log("thread is dead")
 
 def remove(conn):
     if conn in list_of_clients:
+        del nicknames[userId(conn)]
         list_of_clients.remove(conn)
-    print(f"Client {conn.getpeername()} got removed.")
+    server_log(f"Client {conn.getpeername()} got REMOVED.")
+    broadcast( f"Client {conn.getpeername()} has left.", conn )
 
 def run_server():
-    while True:
-        conn, addr = server.accept()
-        list_of_clients.append(conn)
-        print(f"Connection from {addr[0]}, {addr[1]}")
-        _thread.start_new_thread( handle_client, (conn,))
+    server_log("Everything works so far, lets talk.")
+    try:
+        while True:
+            conn, addr = server.accept()
+            list_of_clients.append( conn )
+            server_log(f"Connection from {addr[0]}, {addr[1]}")
+            
+            nicknames[userId(conn)] = str(conn.getpeername()[0]) # temporary first name
+            broadcast(f"{nicknames[userId(conn)]} has just joined", None)
+            NewThread = threading.Thread( target=handle_client, args=(conn,))
+            NewThread.daemon = True
+            NewThread.start()
+    except KeyboardInterrupt:
+        exit()
 
 if __name__ == "__main__":
     run_server()
